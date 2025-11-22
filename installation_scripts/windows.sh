1bd0115947284cce59d3063ce5a50ee5ced93c7a
Lilian Bazantay <lilian.bazantay@epitech.eu>
2025-11-22 18:06:59 +0100

    fix(installer): turn windows script into an actual bash script

--- installation_scripts/windows.sh
+++ installation_scripts/windows.sh
@@ -4,26 +4,25 @@ set -e
 # Install CMake
 winget install --id Kitware.CMake -e --source winget
 
-
 # Bootstrap vcpkg if needed
-if (!(Test-Path "./external/vcpkg/vcpkg.exe")) (
-    Write-Host "Bootstrapping vcpkg..."
-    & "./external/vcpkg/bootstrap-vcpkg.bat"
-)
+if [ ! -f "./external/vcpkg/vcpkg.exe" ]; then
+    echo "Bootstrapping vcpkg..."
+    ./external/vcpkg/bootstrap-vcpkg.bat
+fi
 
 # Clean build folder
-if (Test-Path "./build") (
-    Remove-Item -Recurse -Force "./build"
-)
-New-Item -ItemType Directory -Path "./build"
+if [ -d "./build" ]; then
+    rm -rf "./build"
+fi
+mkdir -p build
 
 # Configure
-cmake -B build -S . `
-    -DCMAKE_TOOLCHAIN_FILE=external/vcpkg/scripts/buildsystems/vcpkg.cmake `
-    -G "Visual Studio 17 2022"  # Adjust to your installed VS generator
+cmake -B build -S . \
+    -DCMAKE_TOOLCHAIN_FILE=external/vcpkg/scripts/buildsystems/vcpkg.cmake \
+    -G "Visual Studio 17 2022"
 
 # Build
 cmake --build build --config Release
 
-cp build/r-type_client r-type_client
-cp build/r-type_server r-type_server
+cp build/r-type_client.exe r-type_client.exe
+cp build/r-type_server.exe r-type_server.exe
