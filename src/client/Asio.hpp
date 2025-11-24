eea82ce8756d46bc1bd3598c32bc1d4cf113dad8
Antoine Quillet <antoine.quillet@epitech.eu>
2025-11-24 14:21:26 +0100

    refactor(Asio class): remove empty spaces at the end of files

--- src/client/Asio.hpp
+++ src/client/Asio.hpp
@@ -9,16 +9,12 @@
 class Asio_network {
     public:
         using ReceiveCallback = std::function<void(const std::string&, const asio::ip::udp::endpoint&)>;
-
         Asio_network(unsigned short listen_port,
                      ReceiveCallback on_receive = nullptr);
-
         ~Asio_network();
 
         void start();
         void stop();
-
-        // Send to a specific address:port
         void send(const std::string& msg,
                   const std::string& host,
                   unsigned short port);
