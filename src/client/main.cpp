c76d2ed26cf578a226e114b8b5accdba5a18ffb1
Tutur <arthur.drahy@epitech.eu>
2025-12-02 15:15:40 +0100

    client connexion + able to discute

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,5 +1,8 @@
 #include "../utils.hpp"
 #include "parser.hpp"
+#include "client.hpp"
+#include <iostream>
+#include <thread>
 
 /**
  * @brief Main function that launch the programe
@@ -9,10 +12,22 @@
  * @return int Return into the terminal EXIT_ERROR(= 84) if the programe fail
  * and EXIT_SUCCESS if the programe didn't fail
  */
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     Parser parser;
 
-    if (parser.ParseData(argc, argv) == EXIT_ERROR)
-        return EXIT_ERROR;
-    return EXIT_ERROR;
+    if (parser.parse(argc, argv) != EXIT_SUCCESS)
+        return 84;
+    Client client(parser.ip, parser.port);
+    client.start();
+    std::cout << "Connected to " << parser.ip << ":" << parser.port << "\n";
+    std::string userInput;
+    while (true) {
+        std::cout << "> ";
+        std::getline(std::cin, userInput);
+        if (userInput == "quit" || userInput == "exit")
+            break;
+        client.send_message(userInput);
+    }
+    return 0;
 }
