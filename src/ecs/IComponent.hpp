bfb24be9baace6f19406cda9fd60777605425dce
Valentin <valentin.roquejofre@epitech.eu>
2025-12-06 14:51:24 +0100

    Feat(IComponent.hpp): add a name, setter and getter to the IComponent class

--- src/ecs/IComponent.hpp
+++ src/ecs/IComponent.hpp
@@ -1,8 +1,15 @@
 #pragma once
 
+#include <string>
+
 class IComponent
 {
     private:
+        std::string _name;
     public:
         ~IComponent() = default;
+
+        // SET/GET
+        std::string getName() const { return _name; }
+        void setName(const std::string& name) { _name = name; }
 };
