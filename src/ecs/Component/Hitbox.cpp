a32cf4ea8586bf9f3cac0655c142c27b8bdf90eb
Valentin <valentin.roquejofre@epitech.eu>
2025-12-06 16:26:00 +0100

    Feat(HitBox): add Hitbox Component

---
+++ src/ecs/Component/Hitbox.cpp
@@ -0,0 +1,140 @@
+#include "Hitbox.hpp"
+
+#include <algorithm>
+
+/**
+ * @brief Construct a new Hitbox object and set its type to HITBOX
+ *
+ * @param sizeX set _sizeX with a float value
+ * @param sizeY set _sizeY with a float value
+ */
+Hitbox::Hitbox(float sizeX, float sizeY) : _sizeX(sizeX), _sizeY(sizeY)
+{
+    _type = ComponentType::HITBOX;
+}
+
+/**
+ * @brief Construct a new Hitbox object and set its type to HITBOX
+ *
+ * @param sizeX set _sizeX with a float value
+ * @param sizeY set _sizeY with a float value
+ * @param layers set all layers
+ * @param masks set all masks
+ */
+Hitbox::Hitbox(float sizeX, float sizeY, std::vector<int> layers, std::vector<int> masks) : _sizeX(sizeX), _sizeY(sizeY), _layers(layers), _masks(masks)
+{
+    _type = ComponentType::HITBOX;
+}
+
+/**
+ * @brief Get the Hitbox Size object
+ *
+ * @return std::pair<float, float> return the size of the Hitbox
+ */
+std::pair<float, float> Hitbox::GetHitboxSize()
+{
+    return std::make_pair(_sizeX, _sizeY);
+}
+
+/**
+ * @brief Set the Hitbox Size object
+ *
+ * @param sizeX set _sizeX with a float value
+ * @param sizeY set _sizeY with a float value
+ */
+void Hitbox::SetHitboxSize(float sizeX, float sizeY)
+{
+    _sizeX = sizeX;
+    _sizeY = sizeY;
+}
+
+/**
+ * @brief Set the Hitbox Size object
+ *
+ * @param size set _sizeY and _sizeX with a pair of float values
+ */
+void Hitbox::SetHitboxSize(std::pair<float, float> size)
+{
+    _sizeX = size.first;
+    _sizeY = size.second;
+}
+
+/**
+ * @brief Add Layer to the list
+ *
+ * @param layer Add a layer to the layers vector
+ */
+void Hitbox::AddLayer(int layer)
+{
+    _layers.push_back(layer);
+}
+
+/**
+ * @brief Sub Layer from the list
+ *
+ * @param layer remove a layer from the layers vector
+ */
+void Hitbox::SubLayer(int layer)
+{
+    _layers.erase(std::remove(_layers.begin(), _layers.end(), layer), _layers.end());
+}
+
+/**
+ * @brief Get the Layers object
+ *
+ * @return std::vector<int> return the layers vector
+ */
+std::vector<int> Hitbox::GetLayers()
+{
+    return _layers;
+}
+
+/**
+ * @brief Set the Layers object
+ *
+ * @param layers set the layers vector
+ */
+void Hitbox::SetLayers(std::vector<int> layers)
+{
+    _layers = layers;
+}
+
+/**
+ * @brief Add Mask to the list
+ *
+ * @param mask Add a mask to the masks vector
+ */
+void Hitbox::AddMask(int mask)
+{
+    _masks.push_back(mask);
+}
+
+/**
+ * @brief Sub Mask from the list
+ *
+ * @param mask remove a mask from the masks vector
+ */
+void Hitbox::SubMask(int mask)
+{
+    _masks.erase(std::remove(_masks.begin(), _masks.end(), mask), _masks.end());
+}
+
+/**
+ * @brief Get the Mask object
+ *
+ * @return std::vector<int> return the masks vector
+ */
+std::vector<int> Hitbox::GetMask()
+{
+    return _masks;
+}
+
+/**
+ * @brief Set the Mask object
+ *
+ * @param mask set the masks vector
+ */
+void Hitbox::SetMask(std::vector<int> mask)
+{
+    _masks = mask;
+}
---
+++ src/ecs/Component/Hitbox.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <vector>
+
+#include "../IComponent.hpp"
+
+/**
+ * @class Hitbox
+ * @brief Represents the Hitbox of a component
+ *
+ * @param _value The Hitbox value
+ */
+class Hitbox : public IComponent
+{
+    private:
+        float _sizeX;
+        float _sizeY;
+        std::vector<int> _layers;
+        std::vector<int> _masks;
+
+    public:
+        Hitbox(float sizeX, float sizeY);
+        Hitbox(float sizeX, float sizeY, std::vector<int> layers, std::vector<int> mask);
+        ~Hitbox() override = default;
+
+        // Size
+        std::pair<float, float> GetHitboxSize();
+        void SetHitboxSize(float sizeX, float sizeY);
+        void SetHitboxSize(std::pair<float, float> size);
+
+        // Layers
+        void AddLayer(int layer);
+        void SubLayer(int layer);
+        std::vector<int> GetLayers();
+        void SetLayers(std::vector<int> layers);
+
+        // Masks
+        void AddMask(int mask);
+        void SubMask(int mask);
+        std::vector<int> GetMask();
+        void SetMask(std::vector<int> mask);
+};
