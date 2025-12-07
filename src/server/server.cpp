f3e6c7159ac7d0b2d83d0cc7dfbca3b94af39540
Lilian Bazantay <lilian.bazantay@epitech.eu>
2025-12-07 18:18:44 +0100

    feat(server): add doxygen documentation

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -5,17 +5,30 @@
 #include <iostream>
 #include <thread>
 
+/**
+ * @brief Construct a new Server:: Server object
+ * 
+ * @param listen_port port on which the server will listen
+ * @param on_receive callback function used when receiving data
+ */
 Server::Server(__uint16_t listen_port, ReceiveCallback on_receive)
     : socket_(io_ctx_, asio::ip::udp::endpoint(asio::ip::udp::v4(), listen_port))
 {
     this->receive_callback_ = on_receive;
 }
 
-
+/**
+ * @brief Destroy the Server:: Server object
+ * 
+ */
 Server::~Server() {
     stop();
 }
 
+/**
+ * @brief server's runner
+ * 
+ */
 void Server::run() {
     try {
         io_ctx_.run();
@@ -24,6 +37,10 @@ void Server::run() {
     }
 }
 
+/**
+ * @brief server's starter function
+ * 
+ */
 void Server::start() {
     if (running_)
         return;
@@ -32,6 +49,10 @@ void Server::start() {
     io_thread_ = std::jthread(&Server::run, this);
 }
 
+/**
+ * @brief server stopping function
+ * 
+ */
 void Server::stop() {
     if (!running_)
         return;
@@ -40,6 +61,10 @@ void Server::stop() {
     socket_.close();
 }
 
+/**
+ * @brief server's receiver function.
+ * 
+ */
 void Server::do_receive() {
     socket_.async_receive_from(
         asio::buffer(recv_buffer_),
@@ -61,6 +86,13 @@ void Server::do_receive() {
 }
 
 
+/**
+ * @brief server's sender function. It will send the msg contained to the host at a certain port
+ * 
+ * @param msg message to send
+ * @param host host's IP
+ * @param port host's port
+ */
 void Server::send(const std::string& msg, const std::string& host, __uint16_t port)
 {
     asio::ip::udp::endpoint endpoint(
