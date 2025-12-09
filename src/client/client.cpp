9546942ca753cd01f093ad6dd7fb3074bbba9948
Tutur <arthur.drahy@epitech.eu>
2025-12-09 20:19:16 +0100

    feat(client): communication beetween client and server for client

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,15 +1,27 @@
 #include "client.hpp"
 #include <iostream>
+#include "PacketCodec.hpp"
 
-static void on_receive_callback(const std::string &text, const asio::ip::udp::endpoint &sender)
+static uint16_t GLOBAL_PACKET_ID = 0;
+
+static void on_receive_callback(const uint8_t *data,
+                                size_t size,
+                                const asio::ip::udp::endpoint &)
 {
-    std::cout << "[SERVER] " << text << "\n";
+    ServerPacket pkt = decodeServerPacket(data, size);
+
+    std::cout << "\n--- SERVER PACKET ---\n";
+    std::cout << "packetId = " << pkt.packetId << "\n";
+    std::cout << "action   = " << (int)pkt.actionType << "\n";
+    std::cout << "entity   = " << (int)pkt.entityType << "\n";
+    std::cout << "entityId = " << pkt.entityId << "\n";
+    std::cout << "pos      = (" << pkt.posX << ", " << pkt.posY << ")\n";
 }
 
 Client::Client(const std::string &ip, int port)
     : ip_(ip),
-    port_(port),
-    network_(0, on_receive_callback)
+      port_(port),
+      network_(0, on_receive_callback)
 {
 }
 
@@ -19,7 +31,60 @@ void Client::start()
     network_.start();
 }
 
-void Client::send_message(const std::string &msg)
+void Client::sendPacket(const Packet &p)
 {
-    network_.send(msg, ip_, port_);
+    network_.send(std::string((char*)p.bytes.data(), p.size), ip_, port_);
+
+    std::cout << "[CLIENT] Sent packet (" << p.size << " bytes)\n";
+}
+
+void Client::sendInput(bool pressed, uint8_t inputCode)
+{
+    uint8_t action =
+        pressed ? INPUT_PRESSED : INPUT_RELEASED;
+
+    uint8_t payload[1] = { inputCode };
+
+    Packet p = encodeClientPacket(
+        GLOBAL_PACKET_ID++,
+        action,
+        1,
+        payload
+    );
+
+    sendPacket(p);
+}
+
+void Client::sendStartGame()
+{
+    Packet p = encodeClientPacket(
+        GLOBAL_PACKET_ID++,
+        START_GAME,
+        0,
+        nullptr
+    );
+
+    sendPacket(p);
+}
+
+void Client::sendConnectionRequest(uint32_t ipValue, uint16_t portValue)
+{
+    uint8_t payload[6] = {
+        (uint8_t)((ipValue >> 24) & 0xFF),
+        (uint8_t)((ipValue >> 16) & 0xFF),
+        (uint8_t)((ipValue >> 8) & 0xFF),
+        (uint8_t)( ipValue       & 0xFF),
+
+        (uint8_t)((portValue >> 8) & 0xFF),
+        (uint8_t)( portValue       & 0xFF)
+    };
+
+    Packet p = encodeClientPacket(
+        GLOBAL_PACKET_ID++,
+        PLAYER_CONNECT,
+        6,
+        payload
+    );
+
+    sendPacket(p);
 }
