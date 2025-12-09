8012ff380ee482779ac5b6aea7b2246157c3c57e
Valentin <valentin.roquejofre@epitech.eu>
2025-12-09 16:44:54 +0100

    Refactor(AnimatedSprite): replace link by sf::Sprite with IntRect

--- src/ecs/Component/AnimatedSprite.cpp
+++ src/ecs/Component/AnimatedSprite.cpp
@@ -1,87 +1,161 @@
 #include "AnimatedSprite.hpp"
 
+#include <stdexcept>
+
 /**
- * @brief Construct a new AnimatedSprite:: AnimatedSprite object
+ * @brief Construct a new AnimatedSprite object
  *
- * @param link is the link of the AnimatedSprite
- * @param size_x set the size x
- * @param size_y set the size y
- * @param number_of_spritex set _number_of_sprite_x
- * @param number_of_sprite_y set _number_of_sprite_y
+ * @param file_path Path to the texture file
+ * @param size_x Initial width
+ * @param size_y Initial height
+ * @param number_of_sprite_x Number of frames in X
+ * @param number_of_sprite_y Number of frames in Y
  */
-AnimatedSprite::AnimatedSprite(std::string link, float size_x, float size_y, int number_of_sprite_x, int number_of_sprite_y) :
-_link(link), _size_x(size_x), _size_y(size_y), _number_of_sprite_x(number_of_sprite_x), _number_of_sprite_y(number_of_sprite_y)
+AnimatedSprite::AnimatedSprite(const std::string& file_path, float size_x, float size_y, int number_of_sprite_x, int number_of_sprite_y)
+    : _number_of_sprite_x(number_of_sprite_x), _number_of_sprite_y(number_of_sprite_y), _animation_rate(10.f)
 {
+    if (!_texture.loadFromFile(file_path))
+        throw std::runtime_error("Failed to load texture: " + file_path);
+
+    _sprite.setTexture(_texture);
+
+    int frameWidth = _texture.getSize().x / _number_of_sprite_x;
+    int frameHeight = _texture.getSize().y / _number_of_sprite_y;
+    _currentRect = sf::IntRect(0, 0, frameWidth, frameHeight);
+    _sprite.setTextureRect(_currentRect);
+
+    _sprite.setScale(size_x / frameWidth, size_y / frameHeight);
     _type = ComponentType::ANIMATED_SPRITE;
 }
 
 /**
- * @brief Get the AnimatedSprite link
+ * @brief Get the SFML sprite
  *
- * @return std::string return the AnimatedSprite link
+ * @return sf::Sprite& Reference to the sprite
  */
-std::string AnimatedSprite::GetLink()
+sf::Sprite& AnimatedSprite::GetSprite()
 {
-    return _link;
+    return _sprite;
 }
 
 /**
- * @brief Replace _link by a new link
+ * @brief Set a new texture from file
  *
- * @param link new link
+ * @param file_path Path to the new texture file
  */
-void AnimatedSprite::SetLink(std::string link)
+void AnimatedSprite::SetTexture(const std::string& file_path)
 {
-    _link = link;
+    if (!_texture.loadFromFile(file_path))
+        throw std::runtime_error("Failed to load texture: " + file_path);
+
+    _sprite.setTexture(_texture, true);
+
+    int frameWidth = _texture.getSize().x / _number_of_sprite_x;
+    int frameHeight = _texture.getSize().y / _number_of_sprite_y;
+    _currentRect = sf::IntRect(0, 0, frameWidth, frameHeight);
+    _sprite.setTextureRect(_currentRect);
 }
 
 /**
- * @brief Return the AnimatedSprite size
+ * @brief Return the sprite size
  *
- * @return std::pair<float, float> First : size_x, Second: size_y
+ * @return std::pair<float, float> First: width, Second: height
  */
-std::pair<float, float> AnimatedSprite::GetSize()
+std::pair<float, float> AnimatedSprite::GetSize() const
 {
-    return {_size_x, _size_y};
+    sf::Vector2f scale = _sprite.getScale();
+    sf::Vector2i texSize = _currentRect.getSize();
+    return {texSize.x * scale.x, texSize.y * scale.y};
 }
 
 /**
- * @brief Set the AnimatedSprite size with a pair
+ * @brief Set the sprite size with a pair
  *
- * @param size First: size_x, Second: size_y
+ * @param size First: width, Second: height
  */
 void AnimatedSprite::SetSize(std::pair<float, float> size)
 {
-    _size_x = size.first;
-    _size_y = size.second;
+    SetSize(size.first, size.second);
 }
 
 /**
- * @brief Set the AnimatedSprite size
+ * @brief Set the sprite size
  *
- * @param size_x Set _size_x
- * @param size_y Set _size_y
+ * @param size_x Width
+ * @param size_y Height
  */
 void AnimatedSprite::SetSize(float size_x, float size_y)
 {
-    _size_x = size_x;
-    _size_y = size_y;
+    sf::Vector2u texSize(_currentRect.width, _currentRect.height);
+    _sprite.setScale(size_x / texSize.x, size_y / texSize.y);
 }
 
 /**
- * @brief Return the number of sprite in the animation
+ * @brief Multiply the sprite size
  *
- * @return std::pair<int, int> First: number of sprite in x, Second: number of sprite in y
+ * @param multiplicator First: width multiplier, Second: height multiplier
  */
-std::pair<int, int> AnimatedSprite::GetNumberOfSprite()
+void AnimatedSprite::multiplySize(std::pair<float, float> multiplicator)
+{
+    sf::Vector2f scale = _sprite.getScale();
+    _sprite.setScale(scale.x * multiplicator.first, scale.y * multiplicator.second);
+}
+
+/**
+ * @brief Multiply the sprite size
+ *
+ * @param multiplicator_x Width multiplier
+ * @param multiplicator_y Height multiplier
+ */
+void AnimatedSprite::multiplySize(float multiplicator_x, float multiplicator_y)
+{
+    sf::Vector2f scale = _sprite.getScale();
+    _sprite.setScale(scale.x * multiplicator_x, scale.y * multiplicator_y);
+}
+
+/**
+ * @brief Divide the sprite size
+ *
+ * @param divisor First: width divisor, Second: height divisor
+ */
+void AnimatedSprite::deviseSize(std::pair<float, float> divisor)
+{
+    if (divisor.first != 0 && divisor.second != 0)
+    {
+        sf::Vector2f scale = _sprite.getScale();
+        _sprite.setScale(scale.x / divisor.first, scale.y / divisor.second);
+    }
+}
+
+/**
+ * @brief Divide the sprite size
+ *
+ * @param divisor_x Width divisor
+ * @param divisor_y Height divisor
+ */
+void AnimatedSprite::deviseSize(float divisor_x, float divisor_y)
+{
+    if (divisor_x != 0 && divisor_y != 0)
+    {
+        sf::Vector2f scale = _sprite.getScale();
+        _sprite.setScale(scale.x / divisor_x, scale.y / divisor_y);
+    }
+}
+
+/**
+ * @brief Return the number of frames in the animation
+ *
+ * @return std::pair<int, int> First: frames in X, Second: frames in Y
+ */
+std::pair<int, int> AnimatedSprite::GetNumberOfSprite() const
 {
     return {_number_of_sprite_x, _number_of_sprite_y};
 }
 
 /**
- * @brief Set the number of sprite in x and y
+ * @brief Set the number of frames in X and Y
  *
- * @param number_of_sprite First: number of sprite in x, Second: number of sprite in y
+ * @param number_of_sprite First: frames in X, Second: frames in Y
  */
 void AnimatedSprite::SetNumberOfSprite(std::pair<int, int> number_of_sprite)
 {
@@ -90,10 +164,10 @@ void AnimatedSprite::SetNumberOfSprite(std::pair<int, int> number_of_sprite)
 }
 
 /**
- * @brief Set the number of sprite in x and y
+ * @brief Set the number of frames in X and Y
  *
- * @param number_of_sprite_x new number of sprite in x
- * @param number_of_sprite_y new number of sprite in y
+ * @param number_of_sprite_x Frames in X
+ * @param number_of_sprite_y Frames in Y
  */
 void AnimatedSprite::SetNumberOfSprite(int number_of_sprite_x, int number_of_sprite_y)
 {
@@ -106,7 +180,7 @@ void AnimatedSprite::SetNumberOfSprite(int number_of_sprite_x, int number_of_spr
  *
  * @return float Current animation rate
  */
-float AnimatedSprite::GetAnimationRate()
+float AnimatedSprite::GetAnimationRate() const
 {
     return _animation_rate;
 }
@@ -114,7 +188,7 @@ float AnimatedSprite::GetAnimationRate()
 /**
  * @brief Set the animation rate
  *
- * @param animation_rate new animation rate
+ * @param animation_rate New animation rate (frames per second)
  */
 void AnimatedSprite::SetAnimationRate(float animation_rate)
 {
@@ -122,70 +196,20 @@ void AnimatedSprite::SetAnimationRate(float animation_rate)
 }
 
 /**
- * @brief Multiply the _size_x and _size_y of the AnimatedSprite
+ * @brief Return whether the sprite is visible
  *
- * @param multiplicator value the size is multiply by. First: size_x, Second: size_y
+ * @return true if visible
+ * @return false if not visible
  */
-void AnimatedSprite::multiplySize(std::pair<float, float> multiplicator)
-{
-    _size_x *= multiplicator.first;
-    _size_y *= multiplicator.second;
-}
-
-/**
- * @brief Multiply the _size_x and _size_y of the AnimatedSprite
- *
- * @param multiplicator_x value _size_x is multiply by
- * @param multiplicator_y value _size_y is multiply by
- */
-void AnimatedSprite::multiplySize(float multiplicator_x, float multiplicator_y)
-{
-    _size_x *= multiplicator_x;
-    _size_y *= multiplicator_y;
-}
-
-/**
- * @brief Divise the _size_x and _size_y of the AnimatedSprite
- *
- * @param divisor value the size is devise by. First: size_x, Second: size_y
- */
-void AnimatedSprite::deviseSize(std::pair<float, float> divisor)
-{
-    if (divisor.first != 0 && divisor.second != 0) {
-        _size_x /= divisor.first;
-        _size_y /= divisor.second;
-    }
-}
-
-/**
- * @brief Divise the _size_x and _size_y of the AnimatedSprite
- *
- * @param divisor_x value _size_x is divise by
- * @param divisor_y value _size_y is divise by
- */
-void AnimatedSprite::deviseSize(float divisor_x, float divisor_y)
-{
-    if (divisor_x != 0 && divisor_y != 0) {
-        _size_x /= divisor_x;
-        _size_y /= divisor_y;
-    }
-}
-
-/**
- * @brief Return _is_visible
- *
- * @return true the animates sprite is visible
- * @return false the animates sprite is not visible
- */
-bool AnimatedSprite::GetVisibility()
+bool AnimatedSprite::GetVisibility() const
 {
     return _is_visible;
 }
 
 /**
- * @brief Set the visibility
+ * @brief Set the sprite visibility
  *
- * @param visibility Set _is_visible
+ * @param visibility Visibility flag
  */
 void AnimatedSprite::SetVisibility(bool visibility)
 {
@@ -193,7 +217,7 @@ void AnimatedSprite::SetVisibility(bool visibility)
 }
 
 /**
- * @brief Set is_visible to true
+ * @brief Make the sprite visible
  */
 void AnimatedSprite::show()
 {
@@ -201,9 +225,46 @@ void AnimatedSprite::show()
 }
 
 /**
- * @brief Set is_visible to false
+ * @brief Hide the sprite
  */
 void AnimatedSprite::hide()
 {
     _is_visible = false;
+}
+
+/**
+ * @brief Update the animation frame based on elapsed time
+ *
+ * @param deltaTime Time elapsed since last update (in seconds)
+ */
+void AnimatedSprite::update(float deltaTime)
+{
+    if (!_is_visible || _animation_rate <= 0.f)
+        return;
+
+    _elapsed_time += deltaTime;
+    float frameTime = 1.f / _animation_rate;
+
+    while (_elapsed_time >= frameTime)
+    {
+        _elapsed_time -= frameTime;
+        _current_frame_x++;
+
+        if (_current_frame_x >= _number_of_sprite_x)
+        {
+            _current_frame_x = 0;
+            _current_frame_y++;
+            if (_current_frame_y >= _number_of_sprite_y)
+                _current_frame_y = 0;
+        }
+
+        int frameWidth = _texture.getSize().x / _number_of_sprite_x;
+        int frameHeight = _texture.getSize().y / _number_of_sprite_y;
+        _currentRect.left = _current_frame_x * frameWidth;
+        _currentRect.top = _current_frame_y * frameHeight;
+        _currentRect.width = frameWidth;
+        _currentRect.height = frameHeight;
+
+        _sprite.setTextureRect(_currentRect);
+    }
 }
--- src/ecs/Component/AnimatedSprite.hpp
+++ src/ecs/Component/AnimatedSprite.hpp
@@ -2,49 +2,55 @@
 
 #include "../IComponent.hpp"
 
-#include <string>
+#include <SFML/Graphics.hpp>
 
-/**
- * @brief Class for an AnimatedSprite like a player or ennnemy
- */
 class AnimatedSprite : public IComponent
 {
 private:
-    std::string _link;
-    float _size_x;
-    float _size_y;
+    sf::Texture _texture;
+    sf::Sprite _sprite;
+    sf::IntRect _currentRect;
     int _number_of_sprite_x;
     int _number_of_sprite_y;
     float _animation_rate;
+    float _elapsed_time = 0.f;
+    int _current_frame_x = 0;
+    int _current_frame_y = 0;
     bool _is_visible = true;
 
 public:
-    AnimatedSprite(std::string link, float size_x, float size_y, int number_of_sprite_x, int number_of_sprite_y);
+    // ANIMATED SPRITE
+    AnimatedSprite(const std::string& file_path, float size_x, float size_y, int number_of_sprite_x, int number_of_sprite_y);
     ~AnimatedSprite() override = default;
 
-    // SET / GET
-    std::string GetLink();
-    void SetLink(std::string link);
+    // GET / SET
+    sf::Sprite& GetSprite();
+    void SetTexture(const std::string& file_path);
 
-    std::pair<float, float> GetSize();
+    // SIZE
+    std::pair<float, float> GetSize() const;
     void SetSize(std::pair<float, float> size);
     void SetSize(float size_x, float size_y);
-
-    std::pair<int, int> GetNumberOfSprite();
-    void SetNumberOfSprite(std::pair<int, int> number_of_sprite);
-    void SetNumberOfSprite(int number_of_sprite_x, int number_of_sprite_y);
-
-    float GetAnimationRate();
-    void SetAnimationRate(float animation_rate);
-    // OPERATOR
     void multiplySize(std::pair<float, float> multiplicator);
     void multiplySize(float multiplicator_x, float multiplicator_y);
     void deviseSize(std::pair<float, float> divisor);
     void deviseSize(float divisor_x, float divisor_y);
 
+    // NUMBER OF SPRITE
+    std::pair<int, int> GetNumberOfSprite() const;
+    void SetNumberOfSprite(std::pair<int, int> number_of_sprite);
+    void SetNumberOfSprite(int number_of_sprite_x, int number_of_sprite_y);
+
+    // ANIMATION
+    float GetAnimationRate() const;
+    void SetAnimationRate(float animation_rate);
+
     // VISIBILITY
-    bool GetVisibility();
+    bool GetVisibility() const;
     void SetVisibility(bool visibility);
     void show();
     void hide();
+
+    // UPDATE
+    void update(float deltaTime);
 };
