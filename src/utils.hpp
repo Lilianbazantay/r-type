4826c1ad680f400474a2395a8ef7234c7bd1c266
Lilian Bazantay <lilian.bazantay@epitech.eu>
2025-12-09 15:42:53 +0100

    feat(server): add base for dispatcher

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,9 +1,20 @@
 #ifndef UTILS
     #define UTILS
 
+#include <cstddef>
+#include <format>
+#include <string>
+#include <vector>
 #pragma once
 
 constexpr int EXIT_ERROR = 84;
 constexpr int EXIT_DESIRED = 0;
+
+std::string ipToString(std::vector<size_t> ip) {
+    std::string stringIP = "";
+    for (size_t i = 0; i < ip.size(); i++)
+        stringIP += std::format("{}{}", ip[i], (i + 1 < ip.size() ? "." : ""));
+    return stringIP;
+};
 
 #endif
