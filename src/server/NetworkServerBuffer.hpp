7888298fe381bc19c7c819879ca9ad5849e769f0
Antoine Quillet <antoine.quillet@epitech.eu>
2026-01-07 14:22:11 +0100

    feat(NetworkBuffer): add network continuous buffer

--- src/server/NetworkServerBuffer.hpp
+++ src/server/NetworkServerBuffer.hpp
@@ -1,6 +1,7 @@
 
 #pragma once
 
+#include <cstddef>
 #include <vector>
 #include "protocol.hpp"
 #include <mutex>
@@ -36,4 +37,24 @@ class NetworkClientBuffer {
         std::vector<uint8_t> popPacket();
         std::vector<std::vector<uint8_t>> popAllPackets();
         bool empty();
+};
+
+class NetworkContinuousBuffer {
+    private:
+        int serverCurrentId = 0;
+        std::vector<std::vector<uint8_t>> packets = {};
+        std::vector<std::pair<std::pair<int, int>, std::pair<std::vector<uint8_t>, std::vector<uint8_t>>>> entities = {};
+        mutable std::mutex mtx;
+
+        int findEntity(std::pair<int, int>);
+    public:
+        NetworkContinuousBuffer() = default;
+        NetworkContinuousBuffer(int);
+        ~NetworkContinuousBuffer() = default;
+        void addEntity(int, int, std::vector<uint8_t>);
+        void moveEntity(int, int, std::vector<uint8_t>);
+        void deleteEntity(int, int);
+        std::vector<std::vector<uint8_t>> getAllPackets();
+        bool empty();
+        void clear();
 };
