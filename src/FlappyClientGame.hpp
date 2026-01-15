ac238e203ce4ad7176d6f7c36e7101de6f551fd2
Tutur <arthur.drahy@epitech.eu>
2026-01-15 09:54:44 +0100

    feat(flappygame): add bad (but working xd) example of flappygame

---
+++ src/FlappyClientGame.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "client/client.hpp"
+#include "ecs/relevant_data.hpp"
+#include <vector>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+
+
+class FlappyClientGame {
+public:
+    FlappyClientGame(std::string ip, int port, NetworkBuffer* buf);
+    void Loop();
+
+private:
+    void processPackets();
+
+    relevant_data_t data;
+    Client client;
+    NetworkBuffer* netBuf;
+    sf::Texture _texBird;
+    sf::Sprite  _sprBird;
+    sf::Texture _texPipes;
+    sf::Sprite  _sprPipes;
+};
---
+++ src/main_flappy_server.cpp
@@ -0,0 +1,12 @@
+#include "server/ServerGame.hpp"
+#include "FlappyServerGame.hpp"
+
+int main()
+{
+    NetworkServerBuffer rbuf;
+    NetworkClientBuffer sbuf;
+    NetworkContinuousBuffer cbuf;
+    FlappyServerGame game(4242, &rbuf, &sbuf, &cbuf);
+    game.Loop();
+    return 0;
+}
